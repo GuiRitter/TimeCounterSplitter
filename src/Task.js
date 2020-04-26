0141e2bda3e2fa5d767e451ef71d14c7a44b7c25
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2020-04-26 20:24:22 -0300

    formatted time

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
+import { round } from './util';
+
 import * as action from './flux/action';
 
 let moment = require('moment');
@@ -7,6 +10,8 @@ let moment = require('moment');
 class Task extends React.Component {
 
 	render() {
+		let count = moment.duration(this.props.task.count).asHours();
+
 		return <tr><td><input
 			onClick={() => this.props.setActive(this.props.task.name)}
 			type='button'
@@ -17,8 +22,8 @@ class Task extends React.Component {
 			this.props.task.active
 				? 'active'
 				: ''
-		}</td><td>{
-			moment.duration(this.props.task.count).asHours()
+		}</td><td title={count}>{
+			round(count, 1)
 		}</td></tr>;
 	}
 }
---
+++ src/util.js
@@ -0,0 +1,1 @@
+export const round = (value, digits) => Math.floor(value * (10 ** digits)) / (10 ** digits)
